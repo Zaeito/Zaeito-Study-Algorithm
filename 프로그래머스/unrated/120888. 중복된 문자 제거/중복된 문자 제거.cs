bc9a637e8d8b647a7528685e00c55f1d259a67d2
An Hyeong Jin <91716083+Zaeito@users.noreply.github.com>
2023-12-08 19:44:05 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 2.10 ms, Memory: 30.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(string my_string) {
+        string answer = "";
+        answer = string.Concat(my_string.Distinct());
+        return answer;
+    }
+}
