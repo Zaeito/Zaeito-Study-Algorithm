bc8378a51fd5ca9784716e5f1764f1dead2e56a2
An Hyeong Jin <91716083+Zaeito@users.noreply.github.com>
2023-12-24 13:05:40 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.68 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution {
+    public int solution(string my_string) {
+        string[] str = my_string.Split(" ");
+        int answer = int.Parse(str[0]);
+        
+        for(int i = 1; i < str.Length; i++)
+        {
+            if(i % 2 != 0)
+            {
+                if(str[i].Equals("+"))//+ 인지 - 인지 판단
+                {
+                    answer += int.Parse(str[i + 1]);
+                }
+                else
+                {
+                    answer -= int.Parse(str[i + 1]);
+                }
+            }    
+        }
+        return answer;
+    }
+}
